89b47e91c9d67d6a21f3c1ad846cefd831a04309
Inomag <pratiksr12345@gmail.com>
2021-09-10 13:13:08 +0530

    Repository

--- src/main/java/com/boot/rest/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/boot/rest/demo/service/EmployeeServiceImpl.java
@@ -1,40 +1,44 @@
 package com.boot.rest.demo.service;
 
 import com.boot.rest.demo.dao.EmployeeDAO;
+import com.boot.rest.demo.dao.EmployeeRepository;
 import com.boot.rest.demo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class EmployeeServiceImpl{
+public class EmployeeServiceImpl implements EmployeeService{
 
-//    @Autowired
-//    private EmployeeDAO employeeDAO;
-//
-//    @Override
-//    @Transactional
-//    public List<Employee> findAll() {
-//        return employeeDAO.findAll();
-//    }
-//
-//    @Override
-//    @Transactional
-//    public Employee findOne(int id) {
-//        return employeeDAO.findOne(id);
-//    }
-//
-//    @Override
-//    @Transactional
-//    public void save(Employee employee) {
-//        employeeDAO.save(employee);
-//    }
-//
-//    @Override
-//    @Transactional
-//    public void delete(int id) {
-//        employeeDAO.delete(id);
-//    }
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findOne(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee emp = null;
+        if(result.isPresent())emp = result.get();
+        else throw new RuntimeException("Employee not found");
+
+        return emp;
+    }
+
+    @Override
+    public void save(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public void delete(int id) {
+        employeeRepository.deleteById(id);
+    }
 }
