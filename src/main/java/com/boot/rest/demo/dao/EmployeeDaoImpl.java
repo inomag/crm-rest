1a827d7203cd4814b1444db74b631e7d678cb6a8
Inomag <pratiksr12345@gmail.com>
2021-09-10 11:22:25 +0530

    JPA

--- src/main/java/com/boot/rest/demo/dao/EmployeeDaoImpl.java
+++ src/main/java/com/boot/rest/demo/dao/EmployeeDaoImpl.java
@@ -2,12 +2,12 @@ package com.boot.rest.demo.dao;
 
 import com.boot.rest.demo.entity.Employee;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -20,39 +20,70 @@ public class EmployeeDaoImpl implements EmployeeDAO{
         this.entityManager = entityManager;
     }
 
+
+
+    // USING NATIVE HIBERNATE API//////////////////////////////////////////////////////
+//    @Override
+//    public List<Employee> findAll() {
+//        // GET HIBERNATE SESSION
+//        Session session = entityManager.unwrap(Session.class);
+//
+//        // CREATE QUERY
+//        Query<Employee> query = session.createQuery("FROM Employee",Employee.class);
+//
+//        // GET RESULTS
+//        List<Employee> employees = query.getResultList();
+//        //RETURN LIST
+//
+//        return employees;
+//    }
+//
+//    @Override
+//    public Employee findOne(int id) {
+//        Session session = entityManager.unwrap(Session.class);
+//        Employee employee = (Employee)session.get(Employee.class,id);
+//        return employee;
+//    }
+//
+//    @Override
+//    public void save(Employee employee) {
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(employee);
+//    }
+//
+//    @Override
+//    public void delete(int id) {
+//        Session session = entityManager.unwrap(Session.class);
+//        Query query = session.createQuery("DELETE FROM Employee WHERE id=:id");
+//        query.setParameter("id",id);
+//        query.executeUpdate();
+//    }
+
+    /////////////// USING JPA/////////////////////////
+
     @Override
     public List<Employee> findAll() {
-        // GET HIBERNATE SESSION
-        Session session = entityManager.unwrap(Session.class);
-
-        // CREATE QUERY
-        Query<Employee> query = session.createQuery("FROM Employee",Employee.class);
-
-        // GET RESULTS
-        List<Employee> employees = query.getResultList();
-        //RETURN LIST
-
-        return employees;
+        TypedQuery<Employee> query = entityManager.createQuery("from Employee",Employee.class);
+        List<Employee> list = query.getResultList();
+        return list;
     }
 
     @Override
     public Employee findOne(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Employee employee = (Employee)session.get(Employee.class,id);
-        return employee;
+        Employee emp = entityManager.find(Employee.class,id);
+        return emp;
     }
 
     @Override
     public void save(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(employee);
+        Employee emp =entityManager.merge(employee);
+        employee.setId(emp.getId());
     }
 
     @Override
     public void delete(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Query query = session.createQuery("DELETE FROM Employee WHERE id=:id");
-        query.setParameter("id",id);
+        Query query = entityManager.createQuery("delete from Employee where id=:employeeId");
+        query.setParameter("employeeId",id);
         query.executeUpdate();
     }
 }
